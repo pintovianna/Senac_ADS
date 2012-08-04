738d02554ec6ea45380f2f325064309f7434290d
pintovianna <leonardoviannapinto@gmail.com>
2012-08-04 17:27:36 -0300

    Update 2º Semestre/2º - Maior altura/src/Exercicio1/Principal.java

--- 2º Semestre/2º - Maior altura/src/Exercicio1/Principal.java
+++ 2º Semestre/2º - Maior altura/src/Exercicio1/Principal.java
@@ -2,12 +2,12 @@ package Exercicio1;
 
 /**
  * 
- * 1. fazer uma classe que receba várias alturas de pessoas 
- * que estão em uma fila e mostre a maior altura.
+ * 1. fazer uma classe que receba vï¿½rias alturas de pessoas 
+ * que estï¿½o em uma fila e mostre a maior altura.
  * 
  */
 
-public class Principal 
+public class Principal // MARCIO GAYZAO
 {
 
 	public static void main(String[] args) 
@@ -15,14 +15,14 @@ public class Principal
 		OutraClasse p1 = new OutraClasse();
 		p1.setCor("branca");
 		p1.setAltura(2.30);
-		System.out.println("A porta 1 é da cor/altura : " + p1.getCor() + " / " + p1.getAltura());
+		System.out.println("A porta 1 ï¿½ da cor/altura : " + p1.getCor() + " / " + p1.getAltura());
 		p1.Aberta();
 		System.out.printf("Abrir a porta 1: = %b\n", p1.aberta);
 		p1.Fechada();
 
 		OutraClasse p2 = new OutraClasse();
 		p2.setCor("verde");
-		System.out.println("Cor da porta 2 é: " + p2.getCor());
+		System.out.println("Cor da porta 2 ï¿½: " + p2.getCor());
 	
 	}	
 	
